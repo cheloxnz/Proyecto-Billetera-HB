448255ca86526d08f59baf343a211449fdbb02ef
MakeNoise01 <marianomotos50@gmail.com>
2020-09-26 16:09:34 -0300

    F

--- client/src/screens/Estadisticas.js
+++ client/src/screens/Estadisticas.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { Grid, BarChart, YAxis } from 'react-native-svg-charts'
-import { LinearGradient, Stop, Defs } from 'react-native-svg'
 import { ImageBackground, View, StyleSheet, Text, StatusBar, Alert } from 'react-native';
 import { connect } from 'react-redux'
-import * as scale from 'd3-scale'                      
+import { Path } from 'react-native-svg'
+import { AreaChart, Grid, XAxis, YAxis } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
+import * as scale from 'd3-scale'
 import Constants from 'expo-constants';
 import NavBar from '../components/NavBar';
 import FooterNew from '../components/FooterNew';
@@ -18,43 +19,75 @@ const Chart = ({ navigation, transfersAll, account }) => {
     var fechas = []
     transfersAll?.map(t => {
         if (t.emisor == account.Naccount) {
-            valores.push(-t.Quantity)
-            fechas.push(t.createdAt)
+            valores.push({Quantity: -t.Quantity, date: new Date(t.createdAt)})
+            //fechas.push(t.createdAt)
         }
         if (t.receptor == account.Naccount) {
-            valores.push(t.Quantity)
-            fechas.push(t.createdAt)
+            valores.push({Quantity: t.Quantity, date: new Date(t.createdAt)})
+            //fechas.push(t.createdAt)
         }
     })
     var deReversa = valores.reverse()
-    console.log(valores)
+    deReversa.unshift({Quantity: 0, date: new Date('2018, 07, 24')})
+    console.log(deReversa)
 
-    const data  = deReversa
-  
+    const data = deReversa
+
+    const Line = ({ line }) => (
+        <Path
+            key={'line'}
+            d={line}
+            stroke={'rgb(134, 65, 244)'}
+            fill={'none'}
+        />
+    )
+
     return (
         <ImageBackground
             source={require('../assets/consolidated_dot.png')}
             style={styles.background}>
             <View style={styles.content}>
                 <NavBar navigation={navigation} />
+                <View style={{ height: 200, flexDirection: 'row' }}>
                 <YAxis
                     data={data}
-                    yAccessor={({ index }) => index}
-                    scale={scale.scaleBand}
-                    contentInset={{ top: 10, bottom: 10 }}
-                    spacing={0.2}
-                    formatLabel={(_, index) => data[ index ].label}
+                    style={{  backgroundColor: 'white' }}
+                    contentInset={{ top: 30 }}
+                    svg={{ fontSize: 10, fill: 'black', fontWeight: 'bold' }}
+                    numberOfTicks={ data.length }
+                    yAccessor={ ({ item }) => item.Quantity }
+                    formatLabel={ (value) => value}
                 />
-                <BarChart
-                    style={{ height: 200, backgroundColor: 'white', margin: 15, borderRadius: 7, padding: 5 }}
+                 <View style={{ flex: 1}}>
+                <AreaChart
+                    style={{ height: 200, backgroundColor: 'red' }}
                     data={data}
-                    contentInset={{ top: 20, bottom: 20 }}
-                    svg={{
-                        strokeWidth: 2,
-                        fill: 'black',
-                    }}>
+                    contentInset={{ top: 30, bottom: 30 }}
+                    yAccessor={ ({ item }) => item.Quantity }
+                    xAccessor={ ({ item }) => item.date }
+                    curve={shape.curveNatural}
+                    xScale={ scale.scaleTime }
+                    svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
+                >
                     <Grid />
-                </BarChart>
+                    <Line />
+                </AreaChart>
+                </View>
+            </View>
+                <XAxis
+                    data={ data }
+                    svg={{
+                        fill: 'black',
+                        fontSize: 12,
+                        fontWeight: 'bold',
+                    }}
+                    xAccessor={ ({ item }) => item.date }
+                    formatLabel={ (value) => value.getUTCDate()  + '/' + (value.getMonth() + 1)   }
+                    //scale={ scale.scaleUtc() }
+                    //numberOfTicks={ data.length }
+                    style={{ marginHorizontal: -15, height: 20, backgroundColor: 'white' }}
+                    contentInset={{ left: 35, right: 25 }}
+                />
                 <FooterNew navigation={navigation} />
             </View>
         </ImageBackground>
