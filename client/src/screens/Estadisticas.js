59ce92375d31d4b33d14aef9f63436a018b2d575
Marcelo Del Valle <cheloxnz@gmail.com>
2020-09-27 08:15:45 -0300

    Arreglo footer en estadisticas

--- client/src/screens/Estadisticas.js
+++ client/src/screens/Estadisticas.js
@@ -19,16 +19,16 @@ const Chart = ({ navigation, transfersAll, account }) => {
     var fechas = []
     transfersAll?.map(t => {
         if (t.emisor == account.Naccount) {
-            valores.push({Quantity: -t.Quantity, date: new Date(t.createdAt)})
+            valores.push({ Quantity: -t.Quantity, date: new Date(t.createdAt) })
             //fechas.push(t.createdAt)
         }
         if (t.receptor == account.Naccount) {
-            valores.push({Quantity: t.Quantity, date: new Date(t.createdAt)})
+            valores.push({ Quantity: t.Quantity, date: new Date(t.createdAt) })
             //fechas.push(t.createdAt)
         }
     })
     var deReversa = valores.reverse()
-    deReversa.unshift({Quantity: 0, date: new Date('2018, 07, 24')})
+    deReversa.unshift({ Quantity: 0, date: new Date('2018, 07, 24') })
     console.log(deReversa)
 
     const data = deReversa
@@ -49,40 +49,40 @@ const Chart = ({ navigation, transfersAll, account }) => {
             <View style={styles.content}>
                 <NavBar navigation={navigation} />
                 <View style={{ height: 200, flexDirection: 'row' }}>
-                <YAxis
-                    data={data}
-                    style={{  backgroundColor: 'white' }}
-                    contentInset={{ top: 30 }}
-                    svg={{ fontSize: 10, fill: 'black', fontWeight: 'bold' }}
-                    numberOfTicks={ data.length }
-                    yAccessor={ ({ item }) => item.Quantity }
-                    formatLabel={ (value) => value}
-                />
-                 <View style={{ flex: 1}}>
-                <AreaChart
-                    style={{ height: 200, backgroundColor: 'red' }}
-                    data={data}
-                    contentInset={{ top: 30, bottom: 30 }}
-                    yAccessor={ ({ item }) => item.Quantity }
-                    xAccessor={ ({ item }) => item.date }
-                    curve={shape.curveNatural}
-                    xScale={ scale.scaleTime }
-                    svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
-                >
-                    <Grid />
-                    <Line />
-                </AreaChart>
+                    <YAxis
+                        data={data}
+                        style={{ backgroundColor: 'white' }}
+                        contentInset={{ top: 30 }}
+                        svg={{ fontSize: 10, fill: 'black', fontWeight: 'bold' }}
+                        numberOfTicks={data.length}
+                        yAccessor={({ item }) => item.Quantity}
+                        formatLabel={(value) => value}
+                    />
+                    <View style={{ flex: 1 }}>
+                        <AreaChart
+                            style={{ height: 200, backgroundColor: 'red' }}
+                            data={data}
+                            contentInset={{ top: 30, bottom: 30 }}
+                            yAccessor={({ item }) => item.Quantity}
+                            xAccessor={({ item }) => item.date}
+                            curve={shape.curveNatural}
+                            xScale={scale.scaleTime}
+                            svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
+                        >
+                            <Grid />
+                            <Line />
+                        </AreaChart>
+                    </View>
                 </View>
-            </View>
                 <XAxis
-                    data={ data }
+                    data={data}
                     svg={{
                         fill: 'black',
                         fontSize: 12,
                         fontWeight: 'bold',
                     }}
-                    xAccessor={ ({ item }) => item.date }
-                    formatLabel={ (value) => value.getUTCDate()  + '/' + (value.getMonth() + 1)   }
+                    xAccessor={({ item }) => item.date}
+                    formatLabel={(value) => value.getUTCDate() + '/' + (value.getMonth() + 1)}
                     //scale={ scale.scaleUtc() }
                     //numberOfTicks={ data.length }
                     style={{ marginHorizontal: -15, height: 20, backgroundColor: 'white' }}
