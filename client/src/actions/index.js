c941744bb9aa471d4bbdec6d5af207d11927190c
maticordoba7 <matiascba99@gmail.com>
2020-09-29 14:40:25 -0300

    Merge with master

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Card } from "react-native-paper";
-const ip = '192.168.100.4'
+const ip = 'localhost'
 
 // CONSTANTES DE LAS ACTIONS
 export const REGISTER_USER = "REGISTER_USER";
