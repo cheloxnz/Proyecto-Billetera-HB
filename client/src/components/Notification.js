29f7e6271baa0d5e817da4d80427a023ddaacfb3
Marcelo Del Valle <cheloxnz@gmail.com>
2020-09-29 18:18:18 -0300

    Correcciones en QR

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -4,7 +4,7 @@ import { theme } from '../core/theme';
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 
-const Notification = ({ children }) => <Text><FontAwesome name={'user'} style={styles.imageBell} size={35} /></Text>
+const Notification = ({ children }) => <Text><FontAwesome name={'user'} style={styles.imageBell} size={40} /></Text>
 const styles = StyleSheet.create({
     imageBell: {
         marginRight: 10
